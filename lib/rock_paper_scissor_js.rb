14a7d0d4fba33a001125cdb5a07d20ede5a04136
Sam O'Keefe <samok13@gmail.com>
2016-01-06 09:07:51 -0800

    adding classes

--- lib/rock_paper_scissor_js.rb
+++ lib/rock_paper_scissor_js.rb
@@ -1,5 +1,9 @@
 require "rock_paper_scissor_js/version"
+require_relative "rock_paper_scissor_js/game.rb"
+require_relative "rock_paper_scissor_js/player.rb"
 
 module RockPaperScissorJs
-  # Your code goes here...
+  
+
+
 end
